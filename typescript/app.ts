ba5f591926d6fd211016f06f79280f30b57759a4
Suporte Informática <informatica@ajelmat.local>
2018-10-08 18:04:41 -0300

    secao 3 aula 8

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,7 +1,7 @@
 let message: string ="Help ,e Obi-Wan Kenobe. You're my only hope!";
 console.log(message)
 let episode: number = 4
-episode = '4'
+
 console.log("This is episode " + 4)
 episode = episode + 1
 console.log("Next episode in " + episode)
