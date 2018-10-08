28fc252c9a00327a536d8f74d9b77dba4bf6b29c
Suporte Informática <informatica@ajelmat.local>
2018-10-08 17:59:38 -0300

    Seção 3, Aula 8

---
+++ typescript/app.ts
@@ -0,0 +1,12 @@
+let message: string ="Help ,e Obi-Wan Kenobe. You're my only hope!";
+console.log(message)
+let episode: number = 4
+episode = '4'
+console.log("This is episode " + 4)
+episode = episode + 1
+console.log("Next episode in " + episode)
+
+let favoriteDroid :string
+favoriteDroid = 'BB-8'
+console.log("My favorite droid is " + favoriteDroid)
+
---
+++ typescript/dist/app.js
@@ -0,0 +1,10 @@
+var message = "Help ,e Obi-Wan Kenobe. You're my only hope!";
+console.log(message);
+var episode = 4;
+episode = '4';
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode in " + episode);
+var favoriteDroid;
+favoriteDroid = 'BB-8';
+console.log("My favorite droid is " + favoriteDroid);
